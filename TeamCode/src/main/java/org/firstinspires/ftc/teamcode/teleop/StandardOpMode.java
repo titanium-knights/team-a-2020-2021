6e6c6ce559b43ff9d504882b1600dd8b4b045dae
andrewwu1 <andwu22@bergen.org>
2020-11-05 17:47:36 -0500

    Merge branch 'erez-test-1' of C:\Users\andre\StudioProjects\team-a-2020-2021 with conflicts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/StandardOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/StandardOpMode.java
@@ -34,6 +34,45 @@ public class StandardOpMode extends LinearOpMode {
             MecanumDrive.Motor.Vector2D vector = new MecanumDrive.Motor.Vector2D(strafe, speed);
             drive.move(1, vector, turn);
 
+            // Toggles power and direction of the intake motors.
+            if (gm1.left_bumper.pressed()) intake.togglePower();
+            if (gm1.right_bumper.pressed()) intake.toggleDirection();
+            if (gm1.x.pressed()) shooterIsShooting = !shooterIsShooting;
+            if (gamepad1.a) {
+                shooterServo.setPower(-0.5);
+            } else if (gamepad1.y) {
+                shooterServo.setPower(0.5);
+            } else {
+                shooterServo.setPower(0);
+            }
+            if (gamepad1.dpad_up) {
+                wobble.liftArm();
+            } else if (gamepad1.dpad_down) {
+                wobble.lowerArm();
+            } else {
+                wobble.stopArm();
+            }
+            if (gamepad1.dpad_left) {
+                wobble.release();
+            } else if (gamepad1.dpad_right) {
+                wobble.grab();
+            } else {
+                wobble.stopGrabber();
+            }
+
+            if (shooterIsShooting) shooterMotor.setPower(-1); else shooterMotor.setPower(0);
+
+
+            // Todo: Uncomment this and remove the old code when you want to test the Shooter class
+            // Toggle shooter power and controls the pinball as necessary.
+            if (gm1.x.pressed()) shooter.toggleShooterPower();
+            shooter.nudgeRings(gamepad1.y, gamepad1.a);
+
+
+            // Update the GamepadManagers (should happen at the end or beginning of the loop)
+            gm1.updateAll();
+            gm2.updateAll();
+
         }
     }
 }
