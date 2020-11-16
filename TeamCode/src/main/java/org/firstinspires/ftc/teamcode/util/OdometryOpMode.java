fb9840e569508531db471842e023e71e95710337
eiis1000 <erez.m.israeli@gmail.com>
2020-11-16 17:21:34 -0500

    Start OdometryOpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OdometryOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OdometryOpMode.java
@@ -3,4 +3,24 @@ package org.firstinspires.ftc.teamcode.util;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public abstract class OdometryOpMode extends LinearOpMode {
+
+	@Override
+	public void runOpMode() throws InterruptedException {
+
+		open();
+		waitForStart();
+
+		while (opModeIsActive()) {
+			loop();
+		}
+
+		close();
+	}
+
+	public abstract void open();
+
+	public abstract void loop(Vector2D position, Vector2D angle);
+
+	public abstract void close();
+
 }
