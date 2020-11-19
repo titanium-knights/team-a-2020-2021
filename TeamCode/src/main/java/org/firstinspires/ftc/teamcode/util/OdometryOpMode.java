4122c5104070127e54321eaf0c19880944852cab
eiis1000 <erez.m.israeli@gmail.com>
2020-11-19 16:59:40 -0500

    Add starting methods for position and angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OdometryOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/OdometryOpMode.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.util;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import static java.lang.Math.PI;
+
 public abstract class OdometryOpMode extends LinearOpMode {
 
-    public final double deadWheelCircumference = 1*2*Math.PI;
+    public final double deadWheelCircumference = 1 * 2 * PI;
     public final double distanceVerticalEncoder = 1;
     public final double distanceStrafeEncoder = 1;
 
@@ -15,8 +17,8 @@ public abstract class OdometryOpMode extends LinearOpMode {
 	public void runOpMode() throws InterruptedException {
 
 		// Starting position; change later
-		position = Vector2D.cartesian(0, 0);
-		angle = Math.PI / 2;
+		position = getStartingPosition();
+		angle = getStartingAngle();
 
 		odometryStart();
 		waitForStart();
@@ -33,5 +35,17 @@ public abstract class OdometryOpMode extends LinearOpMode {
 	public abstract void odometryLoop() throws InterruptedException;
 
 	public abstract void odometryClose();
+
+	/**
+	 * To be hard-coded per opmode; returns the initial position of the robot.
+	 */
+	public abstract Vector2D getStartingPosition();
+
+	/**
+	 * To be hard-coded per opmode; returns the initial angle of the robot from positive x. Default is PI/2.
+	 */
+	public double getStartingAngle() {
+		return PI / 2;
+	}
 
 }
