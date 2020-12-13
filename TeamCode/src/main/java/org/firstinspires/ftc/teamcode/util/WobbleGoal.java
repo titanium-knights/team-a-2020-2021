5e4bf37b6fdf6bff2063630b3fa4d9579c14f43e
Anthony Li <me@anli.dev>
2020-12-13 17:40:13 -0500

    Use Servo for WobbleGoal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WobbleGoal.java
@@ -2,27 +2,26 @@ package org.firstinspires.ftc.teamcode.util;
 
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class WobbleGoal {
     public CRServo arm;
-    public CRServo grabber;
+    public Servo grabber;
 
-    public WobbleGoal(CRServo arm, CRServo grabber) {
+    public WobbleGoal(CRServo arm, Servo grabber) {
         this.arm = arm;
         this.grabber = grabber;
     }
 
     public void release() {
-        this.grabber.setPower(0.5);
+        this.grabber.setPosition(1);
     }
 
     public void grab() {
-        this.grabber.setPower(-0.5);
+        this.grabber.setPosition(0);
     }
 
-    public void stopGrabber() {
-        this.grabber.setPower(0);
-    }
+    public void stopGrabber() {}
 
     public void liftArm() {
         this.arm.setPower(1);
@@ -37,6 +36,6 @@ public class WobbleGoal {
     }
 
     public static WobbleGoal standard(HardwareMap hardwareMap) {
-        return new WobbleGoal(hardwareMap.crservo.get("wobble_arm"), null);
+        return new WobbleGoal(hardwareMap.crservo.get("wobble_arm"), hardwareMap.servo.get("wobble"));
     }
 }
