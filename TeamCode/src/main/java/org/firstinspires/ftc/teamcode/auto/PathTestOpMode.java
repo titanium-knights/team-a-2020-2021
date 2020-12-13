a4c0ebea5c2bf7d57a94ad71c2bbf2af3927af38
Anthony Li <me@anli.dev>
2020-12-13 15:51:41 -0500

    change 2nd grab pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathTestOpMode.java
@@ -127,7 +127,7 @@ public class PathTestOpMode extends LinearOpMode {
 
         TrajectoryBuilder nextBuilder = drive.trajectoryBuilder(drive.getPoseEstimate(), Math.PI);
         // nextBuilder.splineToLinearHeading(new Pose2d(12, -11, Math.PI), Math.PI);
-        nextBuilder.splineToLinearHeading(new Pose2d(-41, -11.5, Math.PI), Math.PI);
+        nextBuilder.splineToLinearHeading(new Pose2d(-41, -12.5, Math.PI), Math.PI);
         nextBuilder.addTemporalMarker(0.5, wobbleGoal::lowerArm);
         nextBuilder.addTemporalMarker(2.5, wobbleGoal::stopArm);
         drive.followTrajectory(nextBuilder.build());
