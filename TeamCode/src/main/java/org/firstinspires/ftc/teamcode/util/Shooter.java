20241f6354ed06ba6bb9e7bf72a16318c808d29d
Michael Kasprzak <michaelkasprzak522@gmail.com>
2020-12-21 17:22:38 -0500

    Add encoder to Shooter.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Shooter.java
@@ -23,6 +23,7 @@ public class Shooter {
     public void toggleShooterPower () {
         isShooting = !isShooting;
         shooter.setPower(isShooting ? 1 : 0);
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     /**
