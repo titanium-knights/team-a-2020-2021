177c3b3ce36c6d98ad08ddc4c47553bf66cdaffa
andrewwu1 <andwu22@bergen.org>
2021-01-22 22:54:52 -0500

    basically fully tuned, check translational kP tho

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -43,9 +43,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2.3; // in
+    public static double WHEEL_RADIUS = 1.96; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.5; // in
+    public static double TRACK_WIDTH = 12.6; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01598;
-    public static double kA = 0.00032;
-    public static double kStatic = 0.08;
+    public static double kV = 0.0133;
+    public static double kA = 0.0004;
+    public static double kStatic = 0.09179;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -66,7 +66,7 @@ public class DriveConstants {
      * forces acceleration-limited profiling). All distance units are inches.
      */
     public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(
-            54.66855022514893, 54.66855022514893, 0.0,
+            54.66855022514893, 20, 0.0,
             Math.toRadians(232.02053333333333), Math.toRadians(232.02053333333333), 0.0
     );
 
