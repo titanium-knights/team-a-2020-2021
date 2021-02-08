0c74ae693b18d0eb4d30eb0c0a940b5b1fd9a2f9
Christopher Rodriguez <ckrod16@gmail.com>
2021-02-08 17:23:18 -0500

    Added path.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ChristopherRodriguezPath.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class ChristopherRodriguezPath extends LinearOpMode {
+    @Override public void runOpMode() {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        waitForStart();
+        Trajectory trajectory = drive.trajectoryBuilder(new Pose2d(-60,25, Math.toRadians(0)))
+                .splineToLinearHeading(new Pose2d(10,25, Math.toRadians(0)), Math.toRadians(0))
+                //shoot
+                .splineToLinearHeading(new Pose2d(50,-60, Math.toRadians(0)), Math.toRadians(0))
+                //drop off wobble goal
+                .splineToLinearHeading(new Pose2d(-49,50, Math.toRadians(0)), Math.toRadians(0))
+                .build();
+        drive.setPoseEstimate(trajectory.start());
+        drive.followTrajectory(trajectory);
+    }
+}
