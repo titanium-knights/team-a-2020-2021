2e2c5710036d05ac47e8d880f57f54d9e0cd996e
Anthony Li <me@anli.dev>
2021-02-13 16:58:04 -0500

    teleop annotations aaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/WobbleGoalTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/WobbleGoalTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.util.GamepadManager;
 import org.firstinspires.ftc.teamcode.util.WobbleGoal;
 
+@TeleOp(name = "Wobble Goal Tester")
 public class WobbleGoalTest extends OpMode {
     GamepadManager gm;
     WobbleGoal goal;
