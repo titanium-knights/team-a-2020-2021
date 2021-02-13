6e816f84bd392a7eeb6d6365e26efc3455d35bed
Anthony Li <me@anli.dev>
2021-02-13 16:28:07 -0500

    Flip MecanumDrive directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MecanumDrive.java
@@ -277,7 +277,7 @@ public class MecanumDrive {
     static String[] standardMotorNames = {"mecanum_fl", "mecanum_fr", "mecanum_bl", "mecanum_br"};
     static Motor.Vector2D[] standardMotorVectors = {new Motor.Vector2D(1, 1), new Motor.Vector2D(-1, 1), new Motor.Vector2D(-1, 1), new Motor.Vector2D(1, 1)};
     static Motor.Location[] standardMotorLocations = {Motor.Location.FRONT_LEFT, Motor.Location.FRONT_RIGHT, Motor.Location.BACK_LEFT, Motor.Location.BACK_RIGHT};
-    static DcMotor.Direction[] standardMotorDirections = {DcMotor.Direction.FORWARD, DcMotor.Direction.REVERSE, DcMotor.Direction.FORWARD, DcMotor.Direction.REVERSE};
+    static DcMotor.Direction[] standardMotorDirections = {DcMotor.Direction.REVERSE, DcMotor.Direction.FORWARD, DcMotor.Direction.REVERSE, DcMotor.Direction.FORWARD};
 
     /**
      * Given only a hardware map, constructs a ready-to-use mecanum drive with preset motors.
