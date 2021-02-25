b9a2f3e80f5008981b3bfbd90c76ec438a90a5fb
Anthony Li <me@anli.dev>
2021-02-24 19:46:06 -0500

    not have robot go haywire

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/PathTestOpMode.java
@@ -88,9 +88,9 @@ public class PathTestOpMode extends LinearOpMode {
             box = 1;
         }
 
-        Trajectory shootTrajectory = drive.trajectoryBuilder(new Pose2d(-63, -40, Math.toRadians(180)), Math.toRadians(-45))
-                .splineToLinearHeading(new Pose2d(-24, -50, Math.toRadians(180)), Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(-2, -40, Math.toRadians(180)), Math.toRadians(90))
+        Trajectory shootTrajectory = drive.trajectoryBuilder(new Pose2d(-63, -40, Math.toRadians(180)), Math.toRadians(-75))
+                .splineToLinearHeading(new Pose2d(-24, -58, Math.toRadians(180)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-5, -40, Math.toRadians(180)), Math.toRadians(90))
                 .build();
 
         drive.setPoseEstimate(shootTrajectory.start());
