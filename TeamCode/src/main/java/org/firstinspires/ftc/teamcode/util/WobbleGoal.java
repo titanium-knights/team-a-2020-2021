165762084b0d20b249ab94da18167bd4bf2b172a
andrewwu1 <andwu22@bergen.org>
2021-03-06 21:27:39 -0500

    Servo wobble woooooo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/WobbleGoal.java
@@ -7,38 +7,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class WobbleGoal {
     public Servo grabber;
-    public DcMotor arm;
+    public Servo arm1;
+    public Servo arm2;
+    //public DcMotor arm;
 
-    public WobbleGoal(Servo grabber, DcMotor arm) {
+    public WobbleGoal(Servo grabber, Servo arm1, Servo arm2) {
         this.grabber = grabber;
-        this.arm = arm;
+        //this.arm = arm;
+        this.arm1 = arm1;
+        this.arm2 = arm2;
 
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     public void release() {
         this.grabber.setPosition(0.5);
     }
- 
     public void grab() {
         this.grabber.setPosition(0.05);
     }
-
     public void stopGrabber() {}
 
     public void liftArm() {
+        this.arm1.setPosition(0);
+        this.arm2.setPosition(1);
+    }
+    public void lowerArm() {
+            this.arm1.setPosition(1);
+            this.arm2.setPosition(0);
+        }
+    public void stopArm() {
+    }
+
+/*
+    public void liftArm() {
         arm.setPower(0.6);
     }
-
     public void lowerArm() {
-        arm.setPower(-0.6);
-    }
 
+    }
     public void stopArm() {
         arm.setPower(0);
-    }
+    } */
 
     public static WobbleGoal standard(HardwareMap hardwareMap) {
-        return new WobbleGoal(hardwareMap.servo.get("wobble"), hardwareMap.dcMotor.get("wobble_arm"));
+        //return new WobbleGoal(hardwareMap.servo.get("wobble"), hardwareMap.dcMotor.get("wobble_arm"));
+        return new WobbleGoal(hardwareMap.servo.get("wobble"), hardwareMap.servo.get("arm1" ), hardwareMap.servo.get("arm2"));
+
     }
 }
