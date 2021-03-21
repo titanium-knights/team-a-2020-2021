d3914f98c4fd10b781d37506a4513e8ca57a4bbf
Anthony Li <me@anli.dev>
2021-03-21 14:42:00 -0400

    WoleNewEncoderAuto enhancements
    I love keyoards!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/WobbleNewEncoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/WobbleNewEncoderAuto.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.util.WobbleGoal;
 
+@Autonomous(name = "Wole New Encoder Auto")
 public class WobbleNewEncoderAuto extends LinearOpMode {
 
     @Override
@@ -15,20 +17,22 @@ public class WobbleNewEncoderAuto extends LinearOpMode {
         while (wobble.getPosition() > -2521) {
             sleep(10);
         }
+        wobble.stopArm();
         sleep(3000);
 
         wobble.lowerArm();
         while (wobble.getPosition() > -6069) {
             sleep(10);
         }
+        wobble.stopArm();
         sleep(3000);
 
         wobble.liftArm();
         while (wobble.getPosition() < 0) {
             sleep(10);
         }
+        wobble.stopArm();
         sleep(3000);
 
-        wobble.stopArm();
     }
 }
